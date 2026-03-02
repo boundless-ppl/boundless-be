ee61df71d01277366de5ceff0e12e3b41c788d5c
gracekarinn <gracekarina12@gmail.com>
2026-03-02 23:35:09 +0700

    [GREEN] Implement auth api

---
+++ api/auth_api.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"boundless-be/controller"
+	"boundless-be/middleware"
+	"boundless-be/repository"
+	"boundless-be/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func registerAuthRoutes(router *gin.Engine, userRepo repository.UserRepository) {
+	authService := service.NewAuthService(userRepo)
+	authController := controller.NewAuthController(authService)
+	authMiddleware := middleware.NewAuthMiddleware(authService)
+
+	router.POST("/auth/register", authController.Register)
+	router.POST("/auth/login", authController.Login)
+	router.POST("/auth/logout", authMiddleware.RequireAuth(), authController.Logout)
+}
